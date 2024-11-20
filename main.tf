19926536eb0bce9cbf60d6329e980029f9997431
1stwaves <wafiyulumam@gmail.com>
2024-11-21 02:22:54 +0700

    remove key

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_instance" "web-app" {
     instance_type = "t2.micro"
     vpc_security_group_ids = ["sg-007ae066d890f9744"]
     subnet_id = "subnet-03d05c6942a83c97a"
-    key_name = "ollion-umam"
     tags = {
       Name = "web-app"
       CreationDate = formatdate("DD MMM YYYY hh:mm ZZZ",timestamp())
       Env = "Production"
     }
+  }
 }
